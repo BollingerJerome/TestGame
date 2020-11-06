e9675454ac93a2b109609b6464f76365be2ebff3
Jerome Bollinger <jerome.f.bollinger@outlook.com>
2020-11-06 20:34:15 +0100

    is it bollingerJerome now?

--- src/application/Main.java
+++ src/application/Main.java
@@ -20,7 +20,9 @@ import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
-//üben des dev to master merge
+//no more discreteEngineer
+
+
 
 public class Main extends Application {
 	
